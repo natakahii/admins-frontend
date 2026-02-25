c3fc5f790f8c005c13a3fb35662ae243ef7588bb
sirtheprogrammer <sirtheprogrammer@gmail.com>
2026-02-25 00:56:40 +0000

    fix api calling

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+  import.meta.env.VITE_API_BASE_URL || "https://api.natakahii.com";
 
 export const API_V1 = `${API_BASE_URL}/api/v1`;
 
